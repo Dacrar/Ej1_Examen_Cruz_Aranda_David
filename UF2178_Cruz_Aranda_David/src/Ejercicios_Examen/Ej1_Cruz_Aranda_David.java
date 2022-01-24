c4aa04ff4df2aed89f090555218a9038223c03fd
David <david.cruara@gmail.com>
2022-01-24 12:22:53 +0100

    Ejercicio 1 de examen.

---
+++ UF2178_Cruz_Aranda_David/src/Ejercicios_Examen/Ej1_Cruz_Aranda_David.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package Ejercicios_Examen;
+
+import java.util.Scanner;
+
+/**
+ * @author David
+ *
+ */
+public class Ej1_Cruz_Aranda_David {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		Scanner entrada = new Scanner (System.in);
+		
+	
+		System.out.println("Tu peso en KG:");
+		double peso = entrada.nextDouble();
+		
+		System.out.println("Tu altura en metros");
+		double altura = entrada.nextDouble();
+		
+		
+		double imc = peso / Math.pow(altura,2);
+		
+		String diagnostico;
+		if (imc<18.5) {
+			diagnostico="Bajo peso";
+		}
+		  else if (imc>=18.5 && imc<=24.9) {
+			  diagnostico="peso Normal(Saludable)";
+			
+		} else if (imc>=25 && imc<=29.9) {
+			diagnostico="Sobrepeso";
+		} else if (imc>=30 && imc<=40) {
+			diagnostico="Obesidad premórbida";
+
+		} else {
+			diagnostico="Obesidad";
+		}
+			
+		System.out.printf("El paciente de %.2f kg y %.2f m de altura, tiene un IMC de %.2f y"
+				+ " tiene un diagnóstico de %s",peso,altura,imc,diagnostico);
+
+	}
+
+}
---
+++ UF2178_Cruz_Aranda_David/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author David
+ *
+ */
+module UF2178_Cruz_Aranda_David {
+}
